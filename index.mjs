19c6ed62ad817c000cb45330c68686816c87f98a
takusan23 <takumu1200@gmail.com>
2023-10-25 04:02:21 +0900

    Fuse js で検索してみる

--- index.mjs
+++ index.mjs
@@ -1,33 +1,50 @@
-import fs from 'fs/promises'
+// @ts-check
 
-const handler = async (event, context) => {
+import fs from 'fs/promises'
+import Fuse from 'fuse.js'
+
+const searchHandler = async (event, context) => {
     // クエリパラメータ query を取り出す
-    const { query } = event.queryStringParameters
-    // なければエラー
+    const query = event?.queryStringParameters?.['query']
+    // なければ空を返す
     if (!query) {
         return {
-            statusCode: 400
+            statusCode: 200,
+            body: []
         }
     }
 
     // 読み込んで JSON にする
+    // TODO 時系列じゃない
     const searchJsonFile = await fs.readFile('search.json', 'utf-8')
     const searchJsonObject = JSON.parse(searchJsonFile)
 
-    // 部分一致検索をする
+    // Fuse.js で検索をする
+    const fuse = new Fuse(searchJsonObject, {
+        keys: ['title', 'markdown']
+    })
+    // 検索
     // 10 件まで
-    const findBlogItem = searchJsonObject
-        .filter(blogItem => blogItem['title'].includes(query) || blogItem['markdown'].includes(query))
+    const searchResult = fuse.search(query)
+    // レスポンス用に小さくする
+    const responseResult = searchResult
+        .map(({ item }) => ({
+            title: item['title'],
+            link: item['link'],
+            description: item['markdown'].slice(0, 100)
+        }))
         .slice(0, 10)
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify(findBlogItem)
+        body: responseResult
     }
     return response
 }
 
 // テスト
-// console.log(await handler({ queryStringParameters: { query: 'D.C.4' } }, undefined))
+// const demoResult = await searchHandler({ queryStringParameters: { query: 'android' } }, undefined)
+// console.log(demoResult)
+// await fs.writeFile('result.json', JSON.stringify(demoResult))
 
-export default handler
+export const handler = searchHandler 
